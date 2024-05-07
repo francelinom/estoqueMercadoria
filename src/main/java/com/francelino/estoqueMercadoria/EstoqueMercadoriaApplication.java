69641fd84ad8e6cfb72864c2f58630a0ef93fef0
Francelino <francelino.silva.prestserv@petrobras.com.br>
2024-05-07 16:27:11 -0300

    primeiro commit

---
+++ src/main/java/com/francelino/estoqueMercadoria/EstoqueMercadoriaApplication.java
@@ -0,0 +1,13 @@
+package com.francelino.estoqueMercadoria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstoqueMercadoriaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EstoqueMercadoriaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/francelino/estoqueMercadoria/EstoqueMercadoriaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.francelino.estoqueMercadoria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstoqueMercadoriaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
