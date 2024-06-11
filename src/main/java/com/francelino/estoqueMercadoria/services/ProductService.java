d06fc9a4f2d8f6ceb90fd1a78187400de4794647
Francelino <francelino.silva.prestserv@petrobras.com.br>
2024-06-11 20:26:21 -0300

    Feat: metodo auxiliar para inserir e atualizar o produto

--- src/main/java/com/francelino/estoqueMercadoria/services/ProductService.java
+++ src/main/java/com/francelino/estoqueMercadoria/services/ProductService.java
@@ -1,7 +1,10 @@
 package com.francelino.estoqueMercadoria.services;
 
+import com.francelino.estoqueMercadoria.dto.CategoryDTO;
 import com.francelino.estoqueMercadoria.dto.ProductDTO;
+import com.francelino.estoqueMercadoria.entities.Category;
 import com.francelino.estoqueMercadoria.entities.Product;
+import com.francelino.estoqueMercadoria.repositories.CategoryRepository;
 import com.francelino.estoqueMercadoria.repositories.ProductRepository;
 import com.francelino.estoqueMercadoria.services.exceptions.DataBaseException;
 import com.francelino.estoqueMercadoria.services.exceptions.ResourceNotFoundException;
@@ -22,6 +25,9 @@ public class ProductService {
     @Autowired
     private ProductRepository productRepository;
 
+    @Autowired
+    private CategoryRepository categoryRepository;
+
     @Transactional(readOnly = true)
     public Page<ProductDTO> findAllPaged(PageRequest pageRequest) {
         Page<Product> list = productRepository.findAll(pageRequest);
@@ -38,18 +44,18 @@ public class ProductService {
     }
 
     @Transactional
-    public ProductDTO insert(ProductDTO ProductDTO) {
+    public ProductDTO insert(ProductDTO productDTO) {
         Product product = new Product();
-//        product.setName(ProductDTO.getName());
+        copyDtoToEntity(productDTO, product);
         product = productRepository.save(product);
         return new ProductDTO(product);
     }
 
     @Transactional
-    public ProductDTO update(Long id, ProductDTO ProductDTO) {
+    public ProductDTO update(Long id, ProductDTO productDTO) {
         try {
             Product product = productRepository.getOne(id);
-//            product.setName(ProductDTO.getName());
+            copyDtoToEntity(productDTO, product);
             product = productRepository.save(product);
             return new ProductDTO(product);
         } catch (EntityNotFoundException e) {
@@ -67,6 +73,20 @@ public class ProductService {
             throw new ResourceNotFoundException("Id not found. " + id);
         } catch (DataIntegrityViolationException e) {
             throw new DataBaseException("Integrity violation");
+        }
+    }
+
+    private void copyDtoToEntity(ProductDTO productDTO, Product product) {
+        product.setName(productDTO.getName());
+        product.setDescription(productDTO.getDescription());
+        product.setDate(productDTO.getDate());
+        product.setImgUrl(productDTO.getImgUrl());
+        product.setPrice(productDTO.getPrice());
+
+        product.getCategories().clear();
+        for (CategoryDTO cat : productDTO.getCategories()) {
+            Category category = categoryRepository.getOne(cat.getId());
+            product.getCategories().add(category);
         }
     }
 }
