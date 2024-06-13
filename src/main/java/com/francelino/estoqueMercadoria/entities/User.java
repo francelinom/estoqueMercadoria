faf0ce16a82da741c879203722564cf628237bbd
Francelino <francelino.silva.prestserv@petrobras.com.br>
2024-06-13 15:13:25 -0300

    Feat: mapeamento de user e role e databse seeding

--- src/main/java/com/francelino/estoqueMercadoria/entities/User.java
+++ src/main/java/com/francelino/estoqueMercadoria/entities/User.java
@@ -1,20 +1,33 @@
 package com.francelino.estoqueMercadoria.entities;
 
+import jakarta.persistence.*;
+
 import java.io.Serializable;
 import java.util.HashSet;
 import java.util.Objects;
 import java.util.Set;
 
+
+@Entity
+@Table(name = "tb_user")
 public class User implements Serializable {
 
     private static final long serialVersionUID = 1L;
 
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String firstName;
     private String lastName;
     private String email;
     private String password;
 
+    @ManyToMany
+    @JoinTable(
+            name = "tb_user_role",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id"))
     private Set <Role> roles = new HashSet<>();
 
     public User() {
